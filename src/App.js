8a5553d41a89491698887f0ea65b094dca414eb2
Julian Parra <jparra0417@gmail.com>
2020-03-20 19:24:36 +1300

    JP - Hooks reducer and context

--- src/App.js
+++ src/App.js
@@ -1,36 +1,55 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
 // import HookParent from './components/HookParent';
-import HookMemo from './components/HookMemo';
-// import HookComponentC from './components/HookComponentC';
-// import HookComponentA from './components/HookComponentA';
-// import HookComponentB from './components/HookComponentB';
+// import HookMemo from './components/HookMemo';
+import HookComponentC from './components/HookComponentC';
+import HookComponentA from './components/HookComponentA';
+import HookComponentB from './components/HookComponentB';
 // import HookCounterReduce from './components/HookCounterReduce';
 // import HookCounterComplexReduce from './components/HookCounterComplexReduce';
 
+export const CountContext = React.createContext();
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
+    case 'reset':
+      return initialState;
+    default:
+      return state;
+  }
+}
+
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState);
   return (
     <div className="App">
-      <HookMemo></HookMemo>
+      {/* <HookMemo></HookMemo> */}
       {/* <HookParent></HookParent> */}
       {/* <HookCounterComplexReduce></HookCounterComplexReduce> */}
       {/* <HookCounterReduce></HookCounterReduce> */}
-      {/* <table>
-        <tbody>
-          <tr>
-            <td>
-              <HookComponentA></HookComponentA>
-            </td>
-            <td>
-              <HookComponentB></HookComponentB>
-            </td>
-            <td>
-              <HookComponentC></HookComponentC>
-            </td>
-          </tr>
-        </tbody>
-      </table> */}
+      <CountContext.Provider value={{ countState : count, countDispatch: dispatch}}>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <HookComponentA></HookComponentA>
+              </td>
+              <td>
+                <HookComponentB></HookComponentB>
+              </td>
+              <td>
+                <HookComponentC></HookComponentC>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </CountContext.Provider>
     </div>
   );
 }
--- src/components/HookComponentF.js
+++ src/components/HookComponentF.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { CountContext } from '../App'
 
 
 export default function HookComponentF() {
-
+    const countContext = React.useContext(CountContext)
     return (
         <div>
-            Component f
+            Component f {countContext.countState}
+            <div>
+                <button onClick={() => { countContext.countDispatch('increment') }}>
+                    Increment
+                </button>
+                <button onClick={() => { countContext.countDispatch('decrement') }}>
+                    Decrement
+                </button>
+                <button onClick={() => { countContext.countDispatch('reset') }}>
+                    Reset
+                </button>
+            </div>
         </div>
     )
 }
